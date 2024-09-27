43ca08368f8c9224e3babe027a7da502f0749f91
Kevin Yang <didgmd@live.com>
2024-09-27 14:08:25 +0800

    Add examples in 20240927.

---
+++ 2024-2025-1/20240927/3.10.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    char c1, c2;
+    c1 = getchar();                                 // 从键盘读入一个大写字母，赋给字符变量c1
+    c2 = c1 + 32;                                   // 得到对应的小写字母的ASCII代码，放在字符变量c2中
+    printf("大写字母: %c\n小写字母: %c\n", c1, c2); // 输出c1,c2的值
+    return 0;
+}
---
+++ 2024-2025-1/20240927/3.8.2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a = 66, b = 79, c = 89;
+    putchar(a);
+    putchar(b);
+    putchar(c);
+    putchar('\n');
+    return 0;
+}
